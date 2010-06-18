fa7cb84065d8e972610915f2a0d21ee4e59387ff
Kevin Beswick <kevinbeswick00@gmail.com>
2010-06-18 22:04:13 +0400

    Add empty controllers/views for models

---
+++ app/controllers/addresses_controller.rb
@@ -0,0 +1,11 @@
+class AddressesController < ApplicationController
+  def show
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+end
---
+++ app/controllers/stores_controller.rb
@@ -0,0 +1,14 @@
+class StoresController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+end
