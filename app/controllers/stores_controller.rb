84888b154f85e3194732594256f23ec5f13d50e4
Kevin Beswick <kevinbeswick00@gmail.com>
2010-06-25 22:51:54 +0400

    add a bit of style using the blueprintcss plugin, also add some methods for stores

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -3,10 +3,34 @@ class StoresController < ApplicationController
   end
 
   def show
+   @store = Store.find(params[:id]) 
   end
 
   def new
+    @store = Store.new
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.xml  { render :xml => @store }
+    end
   end
+
+def create
+    @store = Store.new(params[:store])
+    
+    respond_to do |format|
+      if @store.save
+        flash[:notice] = 'Store was successfully added.'
+        format.html { redirect_to(@store) }
+        format.xml  { render :xml => @store, :status => :created, :location => @store }
+      else
+        format.html { render :action => "new" }
+        format.xml  { render :xml => @store.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
+
+
 
   def edit
   end
