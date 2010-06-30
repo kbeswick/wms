10375248fb25a56034d482652427727464e1f874
Kevin Beswick <kevinbeswick00@gmail.com>
2010-06-30 07:26:06 +0400

    add edit and delete buttons, make deletion work

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,5 @@
 class ItemsController < ApplicationController
   def index
-    @registry = Registry.find(params[:registry_id])
-    @items = @registry.items
   end
 
   def show
@@ -35,35 +33,35 @@ class ItemsController < ApplicationController
   def create
     @registry = Registry.find(params[:registry_id])
     @item = @registry.items.build(params[:item])
+    @item.qtybought = 0;
     if @item.save
-      redirect_to registry_item_url(@registry, @item)
+      redirect_to registry_path(@registry)
     else
       render :action => "new"
     end
   end
 
   def edit
-    @registry = Registry.find(params[:registry_id])
-    @item = @registry.items.find(params[:id])
+    @item = Item.find(params[:id])
   end
 
   def update
-    @registry = Registry.find(params[:registry_id])
     @item = Item.find(params[:id])
+    @registry = Registry.find(@item.registry_id)
     if @item.update_attributes(params[:item])
-      redirect_to registry_item_url(@registry, @item)
+      redirect_to registry_path(@registry)
     else
       render :action => "edit"
     end
   end
 
   def destroy
-    @registry = Registry.find(params[:registry_id])
     @item = Item.find(params[:id])
+    @registry = Registry.find(@item.registry_id)
     @item.destroy
 
     respond_to do |format|
-      format.html { redirect_to registry_items_path(@registry) }
+      format.html { redirect_to registry_path(@registry) }
       format.xml { head :ok }
     end
   end
