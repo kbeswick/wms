a8371210f6d56555e675c9d09324e3efc8f61490
Kevin Beswick <kbeswick@terminus.kevinbeswick.com>
2010-09-30 13:32:19 +0000

    update to rails 3 app, mostly working

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wms::Application.config.secret_token = '41930a470c05891ec62717f1b62bb861ed5f4845476c335496d4433b356d30e1d70031702eac8bd182afedc860180f161ccc9531696069268137c0f94feb7a98'
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,15 +1,8 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key for verifying cookie session data integrity.
-# If you change this key, all old sessions will become invalid!
-# Make sure the secret is at least 30 characters and all random, 
-# no regular words or you'll be exposed to dictionary attacks.
-ActionController::Base.session = {
-  :key         => '_wms_session',
-  :secret      => '391432ca6dc45f964ef54ccce303123cee14f3339c4d4fd9de0a28f886c26558057479ceb2c18b702656dffe84872d63eeca7aaf7c4544e94120397d456b2697'
-}
+Wms::Application.config.session_store :cookie_store, :key => '_wms_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rake db:sessions:create")
-# ActionController::Base.session_store = :active_record_store
+# Wms::Application.config.session_store :active_record_store
