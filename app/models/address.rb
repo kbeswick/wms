fdf8ab0402ed1200431f986449222378d9f8ec20
Kevin Beswick <kevinbeswick00@gmail.com>
2010-10-15 16:15:58 +0000

    add some validators to the user registration

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,6 @@
 class Address < ActiveRecord::Base
   belongs_to :user
+  validates_format_of :phone_number,
+          :message => "Must be a valid telephone number.",
+          :with => /^[\(\)0-9\- \+\.]{10,20}$/ 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,13 @@ class User < ActiveRecord::Base
 
   accepts_nested_attributes_for :addresses, :allow_destroy => :false,
     :reject_if => proc { |attrs| attrs.all? { |k, v| v.blank? } }
+
+  validates :email,
+            :message => "Must be a vaild email address.",
+            :presence => true,
+            :format => { :with => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i } 
+
+  validates_uniqueness_of :login
+  validates_presence_of :first_name
+  validates_presence_of :last_name
 end
