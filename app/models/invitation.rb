e27d744e98374a4381e927f8a0a682d56fa62b97
Kevin Beswick <kevinbeswick00@gmail.com>
2010-10-20 22:54:16 +0000

    add invitations

--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -1,2 +1,5 @@
 class Invitation < ActiveRecord::Base
+  belongs_to :invited_user, :class => "User", :foreign_key => "invited_user_id"
+  belongs_to :inviting_user, :class => "User", :foreign_key => "inviting_user_id"
+  belongs_to :event
 end
--- db/migrate/20101015181952_create_invitations.rb
+++ db/migrate/20101015181952_create_invitations.rb
@@ -1,7 +1,12 @@
 class CreateInvitations < ActiveRecord::Migration
   def self.up
     create_table :invitations do |t|
-
+      
+      t.int                 :invited_user_id
+      t.references          :event
+      t.boolean             :rsvp,       :default => false
+      t.timestamp           :rsvp_date
+      t.int                 :inviting_user_id
       t.timestamps
     end
   end
